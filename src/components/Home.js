8e977d84ea251a352ff5f1ccf365edb7969c6eae
Mihir Narkar <Mihirnarkar19@gmail.com>
2022-09-17 22:19:50 +0530

    issue resolve

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,18 @@
 import React from 'react'
 import Carousel from './Carousel'
 import Navbar from './Navbar'
-<<<<<<< HEAD
 import Search from './Search'
-=======
 import About from './About'
 import Footer from './Footer'
->>>>>>> 650933796531fe5705ec9287d1a4229c4b92f948
 
 function Home() {
   return (
     <>
     <Navbar/>
-<<<<<<< HEAD
     <Carousel/>
     <Search/>
-=======
     <About/>
     <Footer/>
->>>>>>> 650933796531fe5705ec9287d1a4229c4b92f948
     </>
   )
 }
