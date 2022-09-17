979f769f6fa933198ff50ce4b7c1044d629b38ec
Jatin Tiwari <myemail>
2022-09-17 22:41:04 +0530

    testing push

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Carousel from './Carousel'
+import About from './About'
 import Navbar from './Navbar'
 import Search from './Search'
+import Footer from './Footer'
 
 function Home() {
   return (
     <>
     <Navbar/>
+    <Carousel/>
+    <Search/>
     <About/>
     <Footer/>
     </>
