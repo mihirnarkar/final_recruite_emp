c462a50140ed817922844fac8221fc11656c1aa6
Jatin Tiwari <myemail>
2022-09-17 22:45:57 +0530

    category card added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,18 @@
 import React from 'react'
+import Navbar from './Navbar'
 import Carousel from './Carousel'
-import Navbar from './Navbar'
 import Search from './Search'
+import Categories_card from './Categories_card'
 import About from './About'
-import Navbar from './Navbar'
-import Search from './Search'
 import Footer from './Footer'
 
 function Home() {
   return (
-    <>
+    <> 
     <Navbar/>
     <Carousel/>
     <Search/>
+    <Categories_card/>
     <About/>
     <Footer/>
     </>
