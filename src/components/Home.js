567451f723890ab9174b7939944521f42087e134
Mihir Narkar <Mihirnarkar19@gmail.com>
2022-09-17 22:16:58 +0530

    Carousel done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Carousel from './Carousel'
 import Navbar from './Navbar'
+import Search from './Search'
 
 function Home() {
   return (
     <>
     <Navbar/>
+    <Carousel/>
+    <Search/>
     </>
   )
 }
---
+++ src/components/Search.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function Search() {
+  return (
+    <>
+    {/* Search starts */}
+    <div className="container-fluid bg-secondary mb-5 wow fadeIn search" data-wow-delay="0.1s">
+            <div className="container">
+                <div className="row g-2">
+                    <div className="col-md-10">
+                        <div className="row g-2">
+                            <div className="col-md-4">
+                                <input type="text" className="form-control border-0" placeholder="Keyword" />
+                            </div>
+                            <div className="col-md-4">
+                                <select className="form-select border-0">
+                                    <option selected>Category</option>
+                                    <option value="1">Category 1</option>
+                                    <option value="2">Category 2</option>
+                                    <option value="3">Category 3</option>
+                                </select>
+                            </div>
+                            <div className="col-md-4">
+                                <select className="form-select border-0">
+                                    <option selected>Location</option>
+                                    <option value="1">Location 1</option>
+                                    <option value="2">Location 2</option>
+                                    <option value="3">Location 3</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <button className="btn btn-dark border-0 w-100">Search</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    {/* Search ends */}
+    </>
+  )
+}
+
+export default Search
