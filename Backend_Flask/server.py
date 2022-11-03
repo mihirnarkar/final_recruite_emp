c082cadd76bd72f376adf10346cccf370cab810e
Jatin Tiwari <myemail>
2022-11-04 01:08:57 +0530

    application track page and data is saving in databse of apply page

--- Backend_Flask/server.py
+++ Backend_Flask/server.py
@@ -1,27 +1,32 @@
-from unittest import result
-from flask import Flask, redirect, flash,url_for, render_template, request, redirect,flash
+from datetime import date
+from flask import Flask, redirect, flash, url_for, render_template, request, redirect, flash, jsonify
 import mysql.connector
 import resume_word_cloud
+from flask_cors import CORS, cross_origin
+cors = CORS(resources={r"/*": {"origins": "*",
+            "allow_headers": "*", "expose_headers": "*"}})
+
 
 app = Flask(__name__)
+
+CORS(app)
 app.secret_key = "super secret key"
- 
+
 
 test_data = {
-        'Name': "jatin",
-        "Age": "22",
-        "Date": 1,
-        "programming": "python"
-    }
+    'Name': "jatin",
+    "Age": "22",
+    "Date": 1,
+    "programming": "python"
+}
 
 
 @app.route("/", methods=['POST'])
 def get_dates():
+    print("file upload")
+    # formData=request.json
 
-    startDate = request.form.get('startDate')
-    endDate = request.form.get('endDate')
-
-    print(startDate, endDate)
+    # # print(formData)
 
     return "<h1 > Home Page </hi>"
 
@@ -34,7 +39,6 @@ def index():
 @app.route('/data')
 def get_time():
 
-    
     return test_data
 
 
@@ -65,7 +69,8 @@ def login():
             flash("You have been logged in")
             return redirect('http://localhost:3000/userhomepage')
         else:
-        	return redirect('http://localhost:3000/security/signin.html') #If credentials are wrong then it will redirect to the same page
+            # If credentials are wrong then it will redirect to the same page
+            return redirect('http://localhost:3000/security/signin.html')
 
 
 @app.route('/security/signin.html', methods=["POST"])
@@ -108,25 +113,148 @@ def signin():
     return redirect('http://localhost:3000/security/signup1.html')
 
 
-@app.route('/features/Resume_word_cloud.html', methods=["POST"])
+@app.route('/Resume_word_cloud', methods=["POST"])
 def resume_word_cloud_checker():
-    print("resume_word_cloud")
-    job_desc = request.form.get('wordcloud')
+    # print("resume_word_cloud")
+
+    print(request.json['firstName'])
+    job_desc = request.json['job_desc']
+    resume_name = request.json['resume_name']
+    # formData=request.json['formData']
+
     print(job_desc)
+    print(resume_name)
 
     result_matched = resume_word_cloud.start_word_cloud()
 
-    print(result_matched) 
+    print(result_matched)
     # flash("You have been flashed hahaha", "info")
-    
-    return test_data 
+
+    return test_data
+
+
+@app.route('/jobApplypage2', methods=['POST'])
+def job_apply():
+    try:
+        print(request.form.get('name'))
+        print(request.form.get('email'))
+        print(request.form.get('phone'))
+        print(request.form.get('current_location'))
+        print(request.form.get('gender'))
+        print(request.form.get('dob'))
+        print(request.form.get('college_name'))
+        print(request.form.get('passing_year'))
+        print(request.form.get('degree_name'))
+        print(request.form.get('degree_specialization'))
+        print(request.form.get('preferred_location'))
+
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        current_location = request.form.get('current_location')
+        gender = request.form.get('gender')
+        dob = request.form.get('dob')
+        college_name = request.form.get('college_name')
+        passing_year = request.form.get('passing_year')
+        degree_specialization = request.form.get('degree_specialization')
+        preferred_location = request.form.get('preferred_location')
+
+        dates = date.today()
+        # dd/mm/YY
+        dates = dates.strftime("%d/%m/%Y")
+        print(dates)
+
+        if (request.form.get('username') != '' and request.form.get('email') != '' and request.form.get('phone') != '' and request.form.get('current_location')) != '':
+
+            print("came to if condition of applicant data form")
+
+            mydb = mysql.connector.connect(host='sql787.main-hosting.eu',
+                                           database='u844323284_Recruitemp',
+                                           user='u844323284_project',
+                                           password='Recruitemp@1234')
+
+            print(mydb)
+            sql_query = f"INSERT INTO `applicant`(`name`, `email`, `phone _number`, `current_location`, `dob`, `gender`, `college`, `passing_year`, `degree_name`,  `preferred_network`,`date`) VALUES ('{name}','{email}','{phone}','{current_location}','{dob}','{gender}','{college_name}','{passing_year}','{degree_specialization}','{preferred_location}', '{dates}')"
+            print(sql_query)
+            cursor = mydb.cursor()
+            cursor.execute(sql_query)
+            mydb.commit()  # saving the data
+            mydb.close()  # closing the connection
+
+            return redirect('/trackapp')
+
+    except Exception as e:
+        print(e)
+        # If user registration is not okay then it will be go to the same page
+    return redirect('/jobApplypage2')
+
+
+@app.route('/datas',  methods=['GET'])
+def get_times():
+
+    return test_data
+
+
+@app.route('/trackapp', methods=['GET'])
+def track_application():
+
+    # id = request.json['id']
+    # print(id)
+
+    if(1 == 1):
+        mydb = mysql.connector.connect(host='sql787.main-hosting.eu',
+                                            database='u844323284_Recruitemp',
+                                            user='u844323284_project',
+                                            password='Recruitemp@1234')
+
+        sql_query = f"SELECT * FROM `applicant` WHERE `sr`={1}"
+
+        cursor = mydb.cursor()
+        cursor.execute(sql_query)
+        applicant_data = cursor.fetchone()
+        print(applicant_data)
+        mydb.close()
+
+        dates = applicant_data[12]
+
+        data = {
+
+            'Name': "jatin",
+            "Age": "22",
+            # "Date": 1,
+            "programming": "python",
+            "Date": applicant_data[12]
+
+
+        }
+        return data
+
+    return test_data
 
 
 
+# @app.route('/services', method=['GET'])
+# def desired_job():
 
+#     if(1 == 1):
+#         mydb = mysql.connector.connect(host='sql787.main-hosting.eu',
+#                                             database='u844323284_Recruitemp',
+#                                             user='u844323284_project',
+#                                             password='Recruitemp@1234')
 
+#         sql_query = f"INSERT INTO `desired_job`( `name`, `email`, `phone _number`, `exp`, `desired_post`, `address`) VALUES ('{}','{}','{}','{}','{}','{}');"
 
+#         cursor = mydb.cursor()
+#         cursor.execute(sql_query)
+#         applicant_data = cursor.fetchone()
+#         print(applicant_data)
+#         mydb.close()
 
+#         dates = applicant_data[12]
+
+#         return data
+
+#     return test_data
 
 
 # Running app
@@ -134,3 +262,4 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 #  "proxy":"http://localhost:5000/", Enable this to connect Flask API
+CORS(app, expose_headers='Authorization')
